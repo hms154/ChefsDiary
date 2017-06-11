23b8e11ee51f16f80354d6f3352069ebc879af6f
Himanshu Sardesai <himanshusardesai@Himanshus-MBP.home>
2017-06-11 13:12:07 -0500

    added new Recipes page

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import Recipe from './Recipe';
+import RecipesList from './RecipesList'
 
 export default class App extends React.Component {
+
   render() {
     return (
-      <Recipe servings={8}/>
+      <RecipesList/>
     );
   }
 }
---
+++ RecipesList.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import {
+  View,
+  ScrollView
+} from "react-native";
+import { Card, List, ListItem } from "react-native-elements";
+import styles from "./styles";
+
+export default class RecipesList extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+        recipes: [
+            "Murgh Mahkani (Butter Chicken)",
+            "Pav Bhaji",
+            "Palak Paneer",
+            "Kadhai Paneer",
+            "Goat Curry",
+            "Egg Curry",
+            "Kheema Mutter",
+            "Murgh Saagawala",
+            "Seekh kebab",
+            "Chicken Korma",
+            "Chicken Rezala"
+        ]
+    }
+  }
+
+  render() {
+    let pic = require("./Spices.jpg")
+    return (
+      <View style={styles.container}>
+        <ScrollView>
+          <Card containerStyle={styles.card} title="Recipes" image={pic}>
+            {this.AddRecipes()}
+          </Card>
+        </ScrollView>
+      </View>
+    );
+  }
+
+  AddRecipes() {
+    const uuidV1 = require("uuid/v1");
+    return (
+      <List containerStyle={styles.listItem}>
+        {this.state.recipes.map((recipe, i) => {
+          return (
+            <ListItem
+              key={uuidV1()}
+              title={recipe}
+            />
+          );
+        })}
+      </List>
+    );
+  }
+
+}
